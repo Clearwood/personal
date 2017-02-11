9989a63d6e6362585b8d76f57caa4a422a54f682
Keno Budde <kenobudde@yahoo.de>
2017-02-11 21:16:36 +0000

    initial

---
+++ server/public/index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tammo
+ * Date: 11.02.2017
+ * Time: 21:04
+ */
+?>
+    <!Doctype HTML>
+    <html lang="en">
+    <head>
+        <title>Coffee2me - Coming Soon</title>
+        <meta name='copyright' content='Copyright 2016. Coffee2me.'>
+        <meta charset="utf-8">
+        <!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <!--<meta http-equiv="Cache-Control" content="no-cache">-->
+        <link rel="shortcut icon" href="favicon.ico">
+        <!-- CSS -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+        <link rel="stylesheet" href="personal.css">
+        <!-- Scripts -->
+    <!--<script
+            src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous"></script>
+            -->
+        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>-->
+
+    </head>
+    <body>
+<?php include_once("../includes/analytics.php") ?>
+<div class="col s0 l4"></div>
+<div class="col s12 l4">
+<div class="valign-wrapper">
+    <h2 class="valign center-align">Keno Budde</h2>
+    <br/>
+    <div class="divider"></div>
+    <h3 class="valign center-align">keno@budde.ws</h3>
+</div>
+</div>
+<div class="col s0 l4"></div>
+    </body>
+</html>
